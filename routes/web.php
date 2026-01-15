ea4f310ec7b0e1ddb87dd910353a96cdecd7e0c1
Dhrubojyoti Das <dhrubojyoti_das@yahoo.in>
2026-01-15 13:04:24 +0530

    Add routes for ScanController

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ScanController;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/', [ScanController::class, 'qr']);
 Route::get('/scan', [ScanController::class, 'index']);
 Route::post('/scan/verify', [ScanController::class, 'verify'])->middleware('throttle:5,1');
